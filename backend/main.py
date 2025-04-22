4d177f343424d7ce1992f8232a35317bae056dcd
nazarov-tj <aziz@nazarov.tj>
2025-04-22 15:31:41 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["frontend-jb2s.onrender.com"],  # в проде укажи домен фронта
-    ...
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
